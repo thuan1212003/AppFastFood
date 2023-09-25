0d5bb9a85ec6d3a1ff2ef5e673baeee4103473af
haijames32 <feckhai2003@gmail.com>
2023-09-25 22:31:35 +0700

    first

---
+++ app/src/main/java/hainb21127/poly/appfastfood/MainActivity.java
@@ -0,0 +1,14 @@
+package hainb21127.poly.appfastfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
